393613a39c8e502c40d7b0748a432e73aeac813f
Hemant <hemant14j83@gmail.com>
2015-08-23 10:54:33 +0000

    Created devise trainer modal

---
+++ app/models/trainer.rb
@@ -0,0 +1,6 @@
+class Trainer < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :confirmable, :rememberable, :trackable, :validatable
+end
