708d6e08ef426ea9aac939ba70130128ed785d3e
Hemant <hemant14j83@gmail.com>
2015-08-25 06:38:03 +0000

    Adding Homepage and configured routes to redirect to homepage after login

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+  	@trainer=current_trainer
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   
-  devise_for :trainers
+  devise_for :trainers, controller: {registrations: "registrations"}
   resources :expertises
 
   resources :trainers do
 	  resources :expertises
   end
 
+  get "home/index"
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
