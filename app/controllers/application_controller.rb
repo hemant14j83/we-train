cdcafc749023a50308eeefe3e21a3b809a05bdb4
Hemant <hemant14j83@gmail.com>
2015-09-17 11:37:17 +0000

    Fixing Application Controller to let Recruiter and trainer update their respective account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, :configure_r_permitted_parameters, if: :devise_controller?
+  before_filter :configure_r_permitted_parameters, if: :devise_controller?
 
   def stored_location_for(resource)
   	if current_trainer
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   protected
     def configure_r_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) do |t|
-      t.permit(:firm_name,:email, :password, :password_confirmation, :current_password, :firm_type, :firm_number, :firm_address, :address,:city,:state, :country, :pincode, :account_status,:subscribed_plan, :contact_person_name, :contact_person_email, :contact_person_number)
+      t.permit(:firm_name,:email, :password, :password_confirmation, :current_password, :firm_type, :firm_number, :firm_address, :address,:city,:state, :country, :pincode, :account_status,:subscribed_plan, :contact_person_name, :contact_person_email, :contact_person_number,:full_name,:email, :password, :password_confirmation, :current_password, :primary_number, :birth_date,:gender,:address,:city,:state, :country, :profile_status,:subscribed_plan, :busy_from, :busy_till, :image)      
     end
 
     devise_parameter_sanitizer.for(:sign_up) do |t|
@@ -30,18 +30,7 @@ class ApplicationController < ActionController::Base
     end
   end
     
-    def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) do |t|
-      t.permit(:full_name,:email, :password, :password_confirmation, :current_password, :primary_number, :birth_date,:gender,:address,:city,:state, :country, :profile_status,:subscribed_plan, :busy_from, :busy_till)
-    end
-
-    devise_parameter_sanitizer.for(:sign_up) do |t|
-      t.permit(:email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.for(:sign_in) do |t|
-      t.permit(:email, :password)
-    end
-  end
+  
 
   private
     def record_not_found
