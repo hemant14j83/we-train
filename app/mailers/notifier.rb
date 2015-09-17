e112a0b7ea1116a6f1f0403a3929e8f59347ad94
Hemant <hemant14j83@gmail.com>
2015-09-17 09:56:10 +0000

    Adding notification mail, sent based on plan selected

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,35 @@
+class Notifier < ApplicationMailer
+
+  default :from => 'Traix <hemant14j83@gmail.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.verification.subject
+  #
+  def newprogram(program)
+    @program=program
+    mail to: "hemant14j83@gmail.com",  :subject => "New Program -#{@program.name}- added."
+  end
+
+  def newprogram_featured(program)
+    @program=program
+    mail to: "hemant14j83@gmail.com",  :subject => "New Program -#{@program.name}- added."
+  end
+
+  def newprogram_urgent(program)
+    @program=program
+    mail to: "hemant14j83@gmail.com",  :subject => "New Program -#{@program.name}- added."
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.notifier.verification_sent.subject
+  #
+  def send_program_to_trainer(program)
+    @program=program
+    @greeting = "Hi"
+
+    mail to: "hemant14j83@gmail.com", :subject=> "New requirement matching your profile listed on Traix"
+  end
+end
