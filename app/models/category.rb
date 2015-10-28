9d22a2c68d286a1792049a753e6309eacc51817f
Hemant <hemant14j83@gmail.com>
2015-10-28 16:51:24 +0000

    Category: Fixing case sensitive error fixed

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
 	belongs_to :catable, :polymorphic=>true
 	has_many :expertlist
 	validates :name, :presence=>true#, :message=>' can\'t be left blank.'
-	validates_uniqueness_of :name, :message=>' already present.'
+	validates_uniqueness_of :name, :case_sensitive => false, :message=>' already present.'
 end
