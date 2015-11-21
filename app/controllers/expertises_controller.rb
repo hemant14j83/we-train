2f911a4dfb267610fef9dfa745982132728a240e
Hemant <hemant14j83@gmail.com>
2015-11-21 10:17:52 +0000

    Adding Edit Expertise option for trainers

--- app/controllers/expertises_controller.rb
+++ app/controllers/expertises_controller.rb
@@ -6,33 +6,44 @@ class ExpertisesController < ApplicationController
 	@expertise = @trainer.expertises.create(params[:expertise].permit(:expertise_in, :professional_expertise_info, :reference_name, :reference_number, :reference_email, :expertise_verified_status,:reference_name1, :reference_number1, :reference_email1,))
   if @expertise.save
     flash[:notice]='Expertise Added Successfully.'
-    redirect_to trainer_root_path
+    redirect_to '/trainers/profile#showexpert'
   else
     flash[:notice]='Something Went wrong, All fields are mandatory.'
-	  redirect_to trainer_root_path
+	  redirect_to '/trainers/profile#showexpert'
   end
 	#redirect_to edit_trainer_registration_path
  end
 
- def index
- 	@expertise = Expertise.all
+ def index 	
   @trainer=Trainer.find(current_trainer.id)
+  if @trainer.role=='admin'
+    @expertise = Expertise.all
+  elsif @trainer.role=='trainer'
+    @expertise = Expertise.find_by(trainer_id: params[:current_trainer_id])
+    render 'trainerindex'
+  end
  end
 
  def edit
   @expertise=Expertise.find(params[:id])
+  @trainer=Trainer.find(current_trainer.id)
  end
 
  def update
+  @trainer=Trainer.find(current_trainer.id)
   @expertise=Expertise.find(params[:id])
-    respond_to do |format|
-      if @expertise.update(expertise_params)
-        format.html { redirect_to expertise_list_path, notice: 'Details updated successfully.' }
-        format.json { render :show, status: :ok, location: @expertise }
-      else
-        format.html { render :edit }
-        format.json { render json: @expertise.errors, status: :unprocessable_entity }
+    if @expertise.expertise_verified_status=='UnVerified'
+      respond_to do |format|
+        if @expertise.update(params[:expertise].permit(:professional_expertise_info, :reference_name, :reference_number, :reference_email, :expertise_verified_status,:reference_name1, :reference_number1, :reference_email1))
+          format.html { redirect_to '/trainers/profile#showexpert', notice: 'Details updated successfully.' }
+          format.json { render :show, status: :ok, location: @expertise }
+        else
+          format.html { render :edit }
+          format.json { render json: @expertise.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      flash[:notice]='Updating verified expertise is not allowed.'
     end
  end
 
@@ -43,7 +54,7 @@ class ExpertisesController < ApplicationController
  def destroy
  	@expertise = Expertise.find(params[:id])
     @expertise.destroy
-    redirect_to trainer_root_path
+    redirect_to '/trainers/profile#showexpert'
  end
 
  private
